d5c05607890549bd14d730eadeae250db2431eec
Anirudh <ak.no@outlook.com>
2026-05-15 16:41:36 -0400

    Update recoilNexus.tsx

--- lib/recoilNexus.tsx
+++ lib/recoilNexus.tsx
@@ -6,53 +6,53 @@ import { useRecoilCallback, RecoilState } from "recoil";
 type ValOrUpdater<T> = T | ((currVal: T) => T);
 
 const nexus: {
-  get?: <T>(atom: RecoilState<T>) => T;
-  getPromise?: <T>(atom: RecoilState<T>) => Promise<T>;
-  set?: <T>(atom: RecoilState<T>, valOrUpdater: ValOrUpdater<T>) => void;
-  reset?: <T>(atom: RecoilState<T>) => void;
+  get?: <T,>(atom: RecoilState<T>) => T;
+  getPromise?: <T,>(atom: RecoilState<T>) => Promise<T>;
+  set?: <T,>(atom: RecoilState<T>, valOrUpdater: ValOrUpdater<T>) => void;
+  reset?: <T,>(atom: RecoilState<T>) => void;
 } = {};
 
 export default function RecoilNexus() {
   nexus.get = useRecoilCallback(
     ({ snapshot }) =>
-      <T>(atom: RecoilState<T>) =>
+      <T,>(atom: RecoilState<T>) =>
         snapshot.getLoadable(atom).contents,
     [],
   );
   nexus.getPromise = useRecoilCallback(
     ({ snapshot }) =>
-      <T>(atom: RecoilState<T>) =>
+      <T,>(atom: RecoilState<T>) =>
         snapshot.getPromise(atom),
     [],
   );
   nexus.set = useRecoilCallback(
     ({ transact_UNSTABLE }) =>
-      <T>(atom: RecoilState<T>, valOrUpdater: ValOrUpdater<T>) => {
+      <T,>(atom: RecoilState<T>, valOrUpdater: ValOrUpdater<T>) => {
         transact_UNSTABLE(({ set }) => set(atom, valOrUpdater));
       },
     [],
   );
   nexus.reset = useRecoilCallback(
     ({ reset }) =>
-      <T>(atom: RecoilState<T>) =>
+      <T,>(atom: RecoilState<T>) =>
         reset(atom),
     [],
   );
   return null;
 }
 
-export function getRecoil<T>(atom: RecoilState<T>): T {
+export function getRecoil<T,>(atom: RecoilState<T>): T {
   return nexus.get!(atom);
 }
 
-export function getRecoilPromise<T>(atom: RecoilState<T>): Promise<T> {
+export function getRecoilPromise<T,>(atom: RecoilState<T>): Promise<T> {
   return nexus.getPromise!(atom);
 }
 
-export function setRecoil<T>(atom: RecoilState<T>, valOrUpdater: ValOrUpdater<T>): void {
+export function setRecoil<T,>(atom: RecoilState<T>, valOrUpdater: ValOrUpdater<T>): void {
   nexus.set!(atom, valOrUpdater);
 }
 
-export function resetRecoil<T>(atom: RecoilState<T>): void {
+export function resetRecoil<T,>(atom: RecoilState<T>): void {
   nexus.reset!(atom);
 }
